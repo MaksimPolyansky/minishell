c23c3cfa81cfcd154be76a26e616f194319d5842
MaksimPolyansky <54674650+MaksimPolyansky@users.noreply.github.com>
2021-01-21 21:22:53 +0300

    libft v1.2

--- libft/ft_charjoin.c
+++ libft/ft_charjoin.c
@@ -6,27 +6,28 @@
 /*   By: heusebio <heusebio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/19 18:13:34 by heusebio          #+#    #+#             */
-/*   Updated: 2020/12/19 18:16:23 by heusebio         ###   ########.fr       */
+/*   Updated: 2021/01/10 18:47:44 by heusebio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_charjoin(char const *s1, char const s2)
+char	*ft_charjoin(char **s1, char const s2)
 {
 	char	*str1;
 	int		k;
 
-	str1 = (char*)malloc(sizeof(char) * (ft_strlen((char *)s1) + 2));
+	str1 = (char*)malloc(sizeof(char) * (ft_strlen(*s1) + 1) + 1);
 	if (!str1)
 		return (NULL);
 	k = 0;
-	while (s1[k] != '\0')
+	while ((*s1)[k] != '\0')
 	{
-		str1[k] = s1[k];
+		str1[k] = (*s1)[k];
 		k++;
 	}
 	str1[k++] = s2;
 	str1[k] = '\0';
+	free(*s1);
 	return (str1);
 }
