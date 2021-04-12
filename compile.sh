85d87ed507c51688f38525872f7cda7c6797c7b2
MaksimPolyansky <54674650+MaksimPolyansky@users.noreply.github.com>
2021-04-12 20:02:37 +0300

    Delete compile.sh

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#gcc -g -Wall -Wextra -Werror ./sources/main1.c ./sources/parser.c ./sources/lexxer.c ./sources/tokens.c ./sources/parse.c ./libft/libft.a
-
-gcc -g ./sources/main.c ./sources/parser.c ./sources/lexxer.c ./sources/tokens.c ./sources/parse.c ./sources/before_lexxer.c ./libft/libft.a
